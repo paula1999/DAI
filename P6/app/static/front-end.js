efe9954c064abf309d4c83799ab7d6e5f33c90d9
paula1999 <pvillanunez@correo.ugr.es>
2022-01-02 17:27:18 +0100

    Termino P6

--- P6/app/static/front-end.js
+++ P6/app/static/front-end.js
@@ -1,16 +1,20 @@
 function modo_diurno () {
-    document.body.style.color = 'black';
     document.body.style.backgroundColor = "transparent";
+    document.getElementById("tabla").style.backgroundColor = "transparent";
+    document.getElementById("modalAdd").style.backgroundColor = "white";
+    document.getElementById("modalEdit").style.backgroundColor = "white";
+
 }
 
 function modo_nocturno () {
-    document.body.style.color = 'white';
-    document.body.style.backgroundColor = '#333';
+    document.body.style.backgroundColor = 'grey';
+    document.getElementById("tabla").style.backgroundColor = "grey";
+    document.getElementById("modalAdd").style.backgroundColor = "grey";
+    document.getElementById("modalEdit").style.backgroundColor = "grey";
 }
 
 function aumentar_tam() {
     document.body.style.fontSize = "150%"
-
 }
 
 function disminuir_tam() {
@@ -29,20 +33,106 @@ function eliminar(id) {
     });
 }
 
-function editar(id) {
+function updatePokemonItem() {
+    const idInput = document.getElementById("edit-id");
+    const numInput = document.getElementById("edit-num");
+    const nameInput = document.getElementById("edit-name");
+    const imgInput = document.getElementById("edit-img");
+    const typeInput = document.getElementById("edit-type");
+    const heightInput = document.getElementById("edit-height");
+    const weightInput = document.getElementById("edit-weight");
+    const weaknessesInput = document.getElementById("edit-weaknesses");
+
+    const pokemon = {
+        num: numInput.value.trim(),
+        name: nameInput.value.trim(),
+        img: imgInput.value.trim() ? imgInput.value.trim() : "static/pokemon_unknown.png",
+        type: typeInput.value.trim().split(",")[0] ? typeInput.value.trim().split(",")          : ["Desconocido"],
+        height: heightInput.value.trim(),
+        weight: weightInput.value.trim(),
+        weaknesses: weaknessesInput.value.trim().split(",")[0] ? weaknessesInput.value.trim().split(",")          : ["Desconocido"],
+    };
+
     $.ajax({
-        url: '/pokemon/' + id,
+        url: '/pokemon/' + idInput.value.trim(),
         type: 'PUT',
-        dataType: 'json'
+        dataType: 'json',
+        processData: false,
+        contentType: 'application/json',
+        data: JSON.stringify(pokemon),
+
+        success: function(data) {
+            $("tbody").empty();
+            $.getJSON('/pokemon').done(ver_datos)
+            numInput.value = ""
+            nameInput.value = ""
+            imgInput.value = ""
+            typeInput.value = ""
+            heightInput.value = ""
+            weightInput.value = ""
+            weaknessesInput.value = ""
+        },
+        error : function(xhr, status) {
+            alert('Error, compruebe los campos');
+        }
     })
 }
 
-function aniadir(id) {
+function addPokemonItem() {
+    const numInput = document.getElementById("add-num");
+    const nameInput = document.getElementById("add-name");
+    const imgInput = document.getElementById("add-img");
+    const typeInput = document.getElementById("add-type");
+    const heightInput = document.getElementById("add-height");
+    const weightInput = document.getElementById("add-weight");
+    const weaknessesInput = document.getElementById("add-weaknesses");
+
+    const pokemon = {
+        num: numInput.value.trim(),
+        name: nameInput.value.trim(),
+        img: imgInput.value.trim() ? imgInput.value.trim() : "static/pokemon_unknown.png",
+        type: typeInput.value.trim().split(",")[0] ? typeInput.value.trim().split(",")          : ["Desconocido"],
+        height: heightInput.value.trim(),
+        weight: weightInput.value.trim(),
+        weaknesses: weaknessesInput.value.trim().split(",")[0] ? weaknessesInput.value.trim().split(",")          : ["Desconocido"],
+    };
+
     $.ajax({
-        url: '/pokemon/' + id,
+        url: '/pokemon',
         type: 'POST',
-        dataType: 'json'
+        dataType: 'json',
+        processData: false,
+        contentType: 'application/json',
+        data: JSON.stringify(pokemon),
+
+        success: function(data) {
+            $("tbody").empty();
+            $.getJSON('/pokemon').done(ver_datos)
+            numInput.value = ""
+            nameInput.value = ""
+            imgInput.value = ""
+            typeInput.value = ""
+            heightInput.value = ""
+            weightInput.value = ""
+            weaknessesInput.value = ""
+        },
+        error : function(xhr, status) {
+            alert('Error, compruebe los campos');
+        }
     })
+}
+
+function get_pokemon (){
+    $.ajax({
+        url: '/pokemon/',
+        type: 'GET',
+        dataType: 'json',
+        success: function(data){
+            ver_datos(data)
+        }
+    });
+    $("tbody").empty();
+    $.getJSON('/pokemon').done(ver_datos)
 }
 
 function ver_datos(datos){
@@ -52,7 +142,7 @@ function ver_datos(datos){
         let p = JSON.parse(value)
         
         htmlString += `<tr>`
-        htmlString += `<td>${p.num}</td>`
+        htmlString += `<td id="td">${p.num}</td>`
         htmlString += `<td>${p.name}</td>`
         htmlString += `<td><img src="${p.img}" width="120" height="120"></img></div></td>`
         htmlString += `<td>${p.type}</td>`
@@ -81,7 +171,7 @@ function ver_datos(datos){
             htmlString += `<td></td>`
         
         htmlString += `<td>`
-        htmlString += `<a class="btn btn-warning btn-sm" onclick="editar('${p.num}')">Editar</a>`
+        htmlString += `<a href="#editPokemonModal" class="btn btn-warning btn-sm" data-toggle="modal"><span>Editar</span></a>`
         htmlString += `<br/>`
         htmlString += `<a class="btn btn-danger btn-sm" onclick="eliminar('${p.num}')">Eliminar</a>`
         htmlString += `</td>`
@@ -90,48 +180,44 @@ function ver_datos(datos){
     $("tbody").append(htmlString)
 }
 
-function ver(dato){
+function ver(p){
     let htmlString = ''
-    console.log(dato)
+        
+    htmlString += `<tr>`
+    htmlString += `<td>${p.num}</td>`
+    htmlString += `<td>${p.name}</td>`
+    htmlString += `<td><img src="${p.img}" width="120" height="120"></img></div></td>`
+    htmlString += `<td>${p.type}</td>`
+    htmlString += `<td>${p.height}</td>`
+    htmlString += `<td>${p.weight}</td>`
+    htmlString += `<td>${p.weaknesses}</td>`
     
-    let p = dato
-    console.log(p.num)
-        
-        htmlString += `<tr>`
-        htmlString += `<td>${p.num}</td>`
-        htmlString += `<td>${p.name}</td>`
-        htmlString += `<td><img src="${p.img}" width="120" height="120"></img></div></td>`
-        htmlString += `<td>${p.type}</td>`
-        htmlString += `<td>${p.height}</td>`
-        htmlString += `<td>${p.weight}</td>`
-        htmlString += `<td>${p.weaknesses}</td>`
-        
-        if(p.hasOwnProperty('prev_evolution')){
-            htmlString += `<td>`
-            $.each(p.prev_evolution, function(i, v){
-                htmlString += `${v.num}: ${v.name}<br/>`
-            });
-            htmlString += `</td>`
-        }
-        else 
-            htmlString += `<td></td>`
-
-        if(p.hasOwnProperty('next_evolution')){
-            htmlString += `<td>`
-            $.each(p.next_evolution, function(i, v){
-                htmlString += `${v.num}: ${v.name}<br/>`
-            });
-            htmlString += `</td>`
-        }
-        else 
-            htmlString += `<td></td>`
-        
+    if(p.hasOwnProperty('prev_evolution')){
         htmlString += `<td>`
-        htmlString += `<a class="btn btn-warning btn-sm" onclick="editar('${p.id}')">Editar</a>`
-        htmlString += `<br/>`
-        htmlString += `<a class="btn btn-danger btn-sm" onclick="eliminar('${p.id}')">Borrar</a>`
+        $.each(p.prev_evolution, function(i, v){
+            htmlString += `${v.num}: ${v.name}<br/>`
+        });
         htmlString += `</td>`
-        htmlString += `</tr>`
+    }
+    else 
+        htmlString += `<td></td>`
+
+    if(p.hasOwnProperty('next_evolution')){
+        htmlString += `<td>`
+        $.each(p.next_evolution, function(i, v){
+            htmlString += `${v.num}: ${v.name}<br/>`
+        });
+        htmlString += `</td>`
+    }
+    else 
+        htmlString += `<td></td>`
+    
+    htmlString += `<td>`
+    htmlString += `<a href="#editPokemonModal" class="edit" data-toggle="modal">Editar</a>`
+    htmlString += `<br/>`
+    htmlString += `<a class="btn btn-danger btn-sm" onclick="eliminar('${p.id}')">Borrar</a>`
+    htmlString += `</td>`
+    htmlString += `</tr>`
     
     $("tbody").append(htmlString)
 }
@@ -141,11 +227,14 @@ $(function () {
     
     $("#buscar_num").change(function() {
         let value = $(this).val();
-        let url = '/pokemon/' + value
-        console.log(url);
-        $("tbody").empty();
-        
-        $.getJSON(url).done(ver);
+        if(value == '')
+            $.getJSON('/pokemon').done(ver_datos);
+        else{
+            let url = '/pokemon/' + value
+            $("tbody").empty();
+            
+            $.getJSON(url).done(ver);
+        }
     });
     
 });
